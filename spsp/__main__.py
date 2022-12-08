38cb2463873e06c0a3f658fd72890c9e1df5d43b
m-michurov <mikhail.michurov.00@gmail.com>
2022-12-09 02:30:24 +0700

    Fix read_line function: include newline characters Add informative error messages for syntax and evaluation errors

--- spsp/__main__.py
+++ spsp/__main__.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 import io
 import sys
-from typing import Collection
+from typing import Collection, TextIO
 
 from . import Tokenizer
 from .evaluation import evaluate
 from .parser import parse
 from .scope import Scope
 from .special_symbols import SpecialSymbols
+from .errors import SpspEvaluationError, SpspSyntaxError
 
 
 def read_line() -> str:
@@ -16,19 +17,71 @@ def read_line() -> str:
     result = ''
 
     while True:
-        line = input(prompt).rstrip()
+        line = input(prompt)
 
         if (pos := line.find(SpecialSymbols.Semicolon)) >= 0:
             line = line[:pos]
 
-        line = line.strip(SpecialSymbols.Backslash)
-        result += line
+        result += line + '\n'
 
         if result.count(SpecialSymbols.LeftParenthesis) <= result.count(SpecialSymbols.RightParenthesis) \
                 and result.count(SpecialSymbols.LeftSquareBracket) <= result.count(SpecialSymbols.RightSquareBracket):
             return result
 
         prompt = '... '
+
+
+def find_line(input_stream: TextIO, position: int) -> (str, int, int):
+    initial_pos = input_stream.tell()
+
+    read = 0
+    line_no = 1
+
+    line = ''
+    line_start = 0
+    for line in input_stream:
+        read += len(line)
+
+        if read >= position:
+            line_start = read - len(line)
+            break
+
+        line_no += 1
+
+    position_in_line = position - line_start
+    line = line.rstrip()
+
+    tabs_count = line.count('\t')
+    line = line.replace('\t', ' ' * 4)
+
+    position_in_line = position_in_line - tabs_count + 4 * tabs_count
+
+    input_stream.seek(initial_pos)
+
+    return line, line_no, position_in_line
+
+
+def print_error_message(
+        input_stream: TextIO,
+        file_name: str,
+        error: SpspEvaluationError | SpspSyntaxError,
+        stream: TextIO = sys.stderr
+) -> None:
+    sys.stdout.flush()
+    initial_pos = input_stream.tell()
+    input_stream.seek(0)
+
+    line, line_number, position_in_line = find_line(input_stream, error.position)
+
+    if isinstance(error, SpspEvaluationError):
+        error = error.cause
+
+    print(f'File "{file_name}", line {line_number}', file=stream)
+    print(line, file=stream)
+    print(' ' * position_in_line + '^', file=stream)
+    print(f'{type(error).__name__}: {error}', file=stream)
+
+    input_stream.seek(initial_pos)
 
 
 def run_repl() -> None:
@@ -39,8 +92,8 @@ def run_repl() -> None:
             try:
                 for expression in parse(Tokenizer(input_stream)):
                     print(evaluate(expression, scope))
-            except Exception as e:
-                print(f'{type(e).__name__}: {e}')
+            except (SpspEvaluationError, SpspSyntaxError) as e:
+                print_error_message(input_stream, '<stdin>', e, stream=sys.stdout)
 
 
 def run_files(file_names: Collection[str]) -> None:
@@ -48,8 +101,12 @@ def run_files(file_names: Collection[str]) -> None:
 
     for file_name in file_names:
         with open(file_name, mode='rt', encoding='utf-8') as file:
-            for expression in parse(Tokenizer(file)):
-                evaluate(expression, scope)
+            try:
+                for expression in parse(Tokenizer(file)):
+                    evaluate(expression, scope)
+            except (SpspEvaluationError, SpspSyntaxError) as e:
+                print_error_message(file, file_name, e)
+                break
 
 
 def _main(args: Collection[str]) -> None:
