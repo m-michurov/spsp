098b1862600b266889f84b6bb81f9eb6dd5c32a5
m-michurov <mikhail.michurov.00@gmail.com>
2022-12-21 11:45:47 +0700

    Implement overloads for macros and functions

--- spsp/function.py
+++ spsp/function.py
@@ -1,30 +1,53 @@
 from dataclasses import dataclass
-from typing import Collection, Any, Callable
+from typing import Any
 
 from . import Expression
+from .errors import SpspInvalidBindingError
+from .evaluation import evaluate
 from .scope import Scope
-from .structural_binding import StructuralBindingTarget, bind_structural
+from .structural_binding import (
+    StructuralBindingTarget,
+    bind_structural,
+    is_variadic,
+    split_variadic_binding_target
+)
 
 __all__ = [
-    'Function'
+    'Function',
+    'Overload'
 ]
 
 
 @dataclass(frozen=True, repr=False)
+class Overload:
+    arguments: StructuralBindingTarget
+    body: Expression.AnyExpression
+
+    def accepts(self, n_args: int) -> bool:
+        variadic = is_variadic(self.arguments)
+
+        if not variadic:
+            return len(self.arguments) == n_args
+
+        arguments, _ = split_variadic_binding_target(self.arguments)
+        return len(arguments) <= n_args
+
+
+@dataclass(frozen=True, repr=False)
 class Function:
-    _arguments: StructuralBindingTarget
-    _body: Expression.AnyExpression
-    _evaluate: Callable[[Expression.AnyExpression, Scope], Any]
+    _overloads: tuple[Overload]
     _closure_scope: Scope
 
     def __call__(self, *args: Any) -> Any:
-        return self._evaluate(self._body, self._bind_arguments(args))
+        overload = next((it for it in self._overloads if it.accepts(len(args))), None)
+
+        if overload is None:
+            raise SpspInvalidBindingError(f'No suitable overload for {len(args)} argument(s)')
+
+        local_scope = self._closure_scope.derive()
+        bind_structural(overload.arguments, args, mutable=False, scope=local_scope)
+        return evaluate(overload.body, local_scope)
 
     @property
     def __name__(self) -> str:
         return repr(self)
-
-    def _bind_arguments(self, args: Collection[Any]) -> Scope:
-        local_scope = self._closure_scope.derive()
-        bind_structural(self._arguments, args, mutable=False, scope=local_scope)
-        return local_scope
--- spsp/special_form.py
+++ spsp/special_form.py
@@ -8,29 +8,44 @@ __all__ = [
     'SpecialFormEvaluationRule',
     'special_form',
     'special_forms',
-    'VARIADIC'
+    'fixed_arguments_count',
+    'variadic'
 ]
-
-VARIADIC = object()
 
 SpecialFormEvaluationRule: TypeAlias = Callable[[tuple[Expression.AnyExpression, ...], Scope], Any]
 _special_forms: dict[str, SpecialFormEvaluationRule] = {}
 
 special_forms: Mapping[str, SpecialFormEvaluationRule] = _special_forms
 
+ArityCheck: TypeAlias = Callable[[str, int], None]
 
-def special_form(name: str, arity: int | Any) -> Callable[[SpecialFormEvaluationRule], SpecialFormEvaluationRule]:
+
+def special_form(
+        name: str,
+        arity_check: ArityCheck) -> Callable[[SpecialFormEvaluationRule], SpecialFormEvaluationRule]:
     def decorator(_evaluator: SpecialFormEvaluationRule) -> SpecialFormEvaluationRule:
         def _evaluator_with_arity_check(
                 arguments: tuple[Expression.AnyExpression, ...],
                 scope: Scope
         ) -> Any:
-            if arity is not VARIADIC and len(arguments) != arity:
-                raise SpspArityError(name, arity, len(arguments))
-
+            arity_check(name, len(arguments))
             return _evaluator(arguments, scope)
 
         _special_forms[name] = _evaluator_with_arity_check
         return _evaluator_with_arity_check
 
     return decorator
+
+
+def fixed_arguments_count(arguments_count: int) -> Callable[[str, int], None]:
+    def _validate(name: str, n_args: int) -> None:
+        if arguments_count == n_args:
+            return
+
+        raise SpspArityError(name, expected=arguments_count, actual=n_args)
+
+    return _validate
+
+
+def variadic() -> Callable[[str, int], None]:
+    return lambda *_: None
